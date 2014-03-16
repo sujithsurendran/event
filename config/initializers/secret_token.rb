7db5b19cf386d88a64d64020b9318e8effaad766
sujithsurendran <sujithsurendran@yahoo.com>
2014-03-16 14:33:39 +0530

    001

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventa::Application.config.secret_key_base = '222ea604c3794f2c24ea1a836a32404748368c2daf619e2a7538d0cfe416661cf22a47df1b4f9ac70a473dde75388f17de0da563fbb4c1cb36caccc2db5225ed'
